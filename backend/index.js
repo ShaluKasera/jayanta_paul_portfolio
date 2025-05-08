98591d11a7f0d353384d6afb0f49db64ed133e31
ShaluKasera <shalukumari93129@gmail.com>
2025-05-09 01:17:52 +0530

    10th

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const editRouter = require('./routes/edit')
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://jayanta-paul-portfolio-awom.vercel.app/",  
+  origin: "http://localhost:5173",  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  
   allowedHeaders: ['Content-Type', 'Authorization'],  
 }));
