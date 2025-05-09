a2fd1b74c17252640368d5dcc09ae9f9efed860b
ShaluKasera <shalukumari93129@gmail.com>
2025-05-09 08:00:19 +0530

    13th

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const userRouter = require('./routes/auth'); 
-const editRouter = require('./routes/edit')
+const userRouter = require("./routes/auth");
+const editRouter = require("./routes/edit");
 
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://jayanta-paul-portfolio.vercel.app",  
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
-  allowedHeaders: ['Content-Type', 'Authorization'],  
-}));
-
+app.use(
+  cors({
+    origin: [
+      "https://jayanta-paul-portfolio.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const connect = require("./connection/connetion");
 connect();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-
-app.use('/user', userRouter);
-app.use('/edit',editRouter);
-
+app.use("/user", userRouter);
+app.use("/edit", editRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
